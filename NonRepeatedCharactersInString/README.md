4b59e095a26f330a2fdc0ec0a3a8e248378a0a9c
Ram Simran <ram@payzello.in>
2020-01-01 23:46:59 +0530

    NonRepeatedCharactersInString

---
+++ NonRepeatedCharactersInString/README.md
@@ -0,0 +1,1 @@
+Write a program to first non-repeated character of String
---
+++ NonRepeatedCharactersInString/Solution.java
@@ -0,0 +1,36 @@
+package NonRepeatedCharactersInString;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(final String[] args) {
+        final Scanner s = new Scanner(System.in);
+        System.out.println("Enter the String ");
+        final String str = s.next();
+        final char[] arr = new char[str.length()];
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = str.charAt(j);
+        }
+        final char found = searchchar(str, arr);
+        System.out.println("The first non repeated char is " + found);
+    }
+
+    public static char searchchar(final String a, final char[] b) {
+        int i = 0;
+        while (i != a.length()) {
+            int count = 0;
+            for (int j = 0; j < b.length; j++) {
+                if (i != j) {
+                    if (a.charAt(i) != b[j]) {
+                        count++;
+                    }
+                }
+                if (count == a.length() - 1) {
+                    return a.charAt(i);
+                }
+            }
+            i++;
+        }
+        return 0;
+    }
+}
